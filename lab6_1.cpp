e75bd00fa4267d27cb6ba9fd68fd3c0ce95502d5
Thanpitchapaiboon <thanpitchapaiboon@gmail.com>
2024-11-29 16:49:52 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,22 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int Even_numbers =0;
+    int Odd_numbers =0;
+    int N;
+    while (N!=0){
+        cout << "Enter an integer: ";
+        cin >> N;
+        if (N!=0){
+            if(N%2==0){
+                Even_numbers+=1; 
+            }else{
+                Odd_numbers+=1;
+            }
+        }
+    
+    } 
+    cout<<"#Even numbers = "<<Even_numbers<<"\n";
+    cout<<"#Odd numbers =  "<<Odd_numbers<<"\n";
     return 0;
 }
